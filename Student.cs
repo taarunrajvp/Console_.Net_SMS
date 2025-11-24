ab5786f3de1d0853f650ce2fd77e7365db3b352b
Taarunraj <taarunrajvp@gmail.com>
2025-11-24 16:09:04 +0530

    Updating student.cs

--- Student.cs
+++ Student.cs
@@ -10,9 +10,13 @@ class Student
     public string GetGrade()
     {
         if (Marks >= 90) return "A+";
+
         else if (Marks >= 80) return "A";
+        
         else if (Marks >= 70) return "B";
+        
         else if (Marks >= 60) return "C";
+        
         else return "F";
     }
 
